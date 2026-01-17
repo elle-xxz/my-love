eada563a12271ad19bb20613f917b3900ed1d66f
elle-xxz <gabi64621@gmail.com>
2026-01-17 19:45:28 -0300

    Add function to display text in HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarTexto(){
+  const texto = document.getElementById("texto");
+
+  texto.innerHTML = `
+    Não foi o acaso que me fez pensar em você.<br><br>
+    Foi o coração, com calma,<br>
+    escolhendo ficar.<br><br>
+    💖
+  `;
+}
